6ec68851b2e5ec1fb46953b4713e4cba71dc5f81
Rodrigo Costa <67449702+rodrigolinux@users.noreply.github.com>
2022-07-02 16:06:56 -0300

    Update telegram_install.sh

--- telegram_install.sh
+++ telegram_install.sh
@@ -19,10 +19,11 @@ fi
 echo
 echo $'-----------------------------------\nInstalando o mensageiro Telegram...'
 echo '-----------------------------------'
-echo 'deb http://ppa.launchpad.net/atareao/telegram/ubuntu xenial main \n
+echo 'deb http://ppa.launchpad.net/atareao/telegram/ubuntu xenial main' \n
 deb-src http://ppa.launchpad.net/atareao/telegram/ubuntu xenial main' > /etc/apt/sources.list.d/telegram.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A9653F936FD5529
 apt update && apt install telegram
+echo	
 echo $'---------------------\nInstalação concluída!'
 echo '---------------------'
 sleep 3
